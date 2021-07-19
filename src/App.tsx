3e0a7c9b79187752dc33d6377619c9696f8d1845
Drew <mail.drew.ross@gmail.com>
2021-07-19 08:46:56 -0700

    Set up app for TS, clean up CRA code

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { ReactElement } from "react";
+
+const App = (): ReactElement => {
+  return (
+    <main className='App'>
+    </main>
+  );
+};
+
+export default App;
