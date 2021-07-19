0cefd0cd72fba1b721171d0b064231ac70c1cbec
Drew <mail.drew.ross@gmail.com>
2021-07-19 10:34:14 -0700

    Render Board component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import { ReactElement } from "react";
 
+import Board from './components/Board/Board';
+
 const App = (): ReactElement => {
   return (
     <main className='App'>
+      <Board />
     </main>
   );
 };
