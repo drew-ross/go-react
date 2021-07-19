b78e960dd8fe4532370ec6fff458b2ed9a66ce55
Drew <mail.drew.ross@gmail.com>
2021-07-19 11:04:42 -0700

    Render rows, Spaces from boardMatrix

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,13 +1,25 @@
 import { ReactElement, useState } from "react";
 import { constructBoardMatrix } from "../../helpers";
 import { BoardMatrix } from "../../types/gameTypes";
+import Space from "../Space/Space";
 
 const Board = (): ReactElement => {
   const [boardMatrix, setBoardMatrix] = useState<BoardMatrix>(
     constructBoardMatrix()
   );
 
-  return <div className='Board'></div>;
+  return (
+    <div className='Board'>
+      <div className='Board-row'>
+        {boardMatrix.length > 0 &&
+          boardMatrix.map((row, coordY) =>
+            row.map((spaceValue, coordX) => (
+              <Space key={`${coordY}, ${coordX}`} value={spaceValue} />
+            ))
+          )}
+      </div>
+    </div>
+  );
 };
 
 export default Board;
