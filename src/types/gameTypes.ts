a2dff09003b16b1927d49382873d542ff1ac8791
Drew <mail.drew.ross@gmail.com>
2021-07-19 12:02:30 -0700

    Add PieceColor type

--- src/types/gameTypes.ts
+++ src/types/gameTypes.ts
@@ -1,6 +1,10 @@
 // Board spaces can be empty (N for null or no piece),
 // have a black piece (B)
 // or a white piece (W)
-export type BoardSpace = "N" | "B" | "W";
+export type BoardSpace = "N" | PieceColor;
+
+export type PieceColor = "B" | "W";
 
 export type BoardMatrix = Array<Array<BoardSpace>>;
+
+export type Coordinates = [number, number];
