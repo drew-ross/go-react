38ecabe2ce8f82f61b7d9bf2a9c978ff4f442a1b
Drew <mail.drew.ross@gmail.com>
2021-07-19 12:03:40 -0700

    Add handleClick, coordinates and button function props

--- src/components/Space/Space.tsx
+++ src/components/Space/Space.tsx
@@ -1,17 +1,23 @@
 import { ReactElement } from "react";
-import { BoardSpace } from "../../types/gameTypes";
+import { BoardSpace, Coordinates } from "../../types/gameTypes";
 
 import "./Space.css";
 
 interface SpaceProps {
   value: BoardSpace;
+  yx: Coordinates;
+  placePiece: (yx: Coordinates) => void;
 }
 
 const Space = (props: SpaceProps): ReactElement => {
-  const { value } = props;
+  const { value, yx, placePiece } = props;
+
+  const handleClick = () => {
+    placePiece(yx);
+  };
 
   return (
-    <button className='Space'>
+    <button className={`Space ${value}`} onClick={handleClick}>
       <span>{value}</span>
     </button>
   );
