466ceb4de505316c5b0b52404c50102ef6139572
Drew <mail.drew.ross@gmail.com>
2021-07-19 10:32:23 -0700

    Add boardMatrix state

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,6 +1,12 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
+import { constructBoardMatrix } from "../../helpers";
+import { BoardMatrix } from "../../types/gameTypes";
 
 const Board = (): ReactElement => {
+  const [boardMatrix, setBoardMatrix] = useState<BoardMatrix>(
+    constructBoardMatrix()
+  );
+
   return <div className='Board'></div>;
 };
 
