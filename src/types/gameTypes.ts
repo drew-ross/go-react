06da3fd77be45d91aa1cf7ac64f8ad15b377fee4
Drew <mail.drew.ross@gmail.com>
2021-07-19 10:33:07 -0700

    Add BoardMatrix type

--- src/types/gameTypes.ts
+++ src/types/gameTypes.ts
@@ -1,4 +1,6 @@
-// Board spaces can be empty (N for null or no piece), 
+// Board spaces can be empty (N for null or no piece),
 // have a black piece (B)
 // or a white piece (W)
-export type BoardSpace = 'N' | 'B' | 'W';
+export type BoardSpace = "N" | "B" | "W";
+
+export type BoardMatrix = Array<Array<BoardSpace>>;
