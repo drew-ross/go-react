3ce0b33c3031f72ae9a632b9d74c9f7f60987e81
Drew <mail.drew.ross@gmail.com>
2021-07-21 09:05:32 -0700

    Extract placePiece to helpers

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,21 +1,55 @@
-import { BoardMatrix, BoardSpace, Coordinates } from "./types/gameTypes";
+import {
+  BoardMatrix,
+  BoardSpace,
+  Coordinates,
+  PieceColor,
+} from "./types/gameTypes";
 
 // Public functions
 
 export const constructBoardMatrix = (size: number = 19): BoardMatrix => {
   return new Array(size).fill(
-    new Array<BoardSpace>(size).fill(["N", null], 0, size), 0, size);
+    new Array<BoardSpace>(size).fill(["N", null], 0, size),
+    0,
+    size
+  );
+};
+
+// Place a piece for the current player if the space is empty
+export const placePiece = (
+  boardMatrix: BoardMatrix,
+  yx: Coordinates,
+  playerTurn: PieceColor,
+  onSuccessCb: () => void
+): BoardMatrix => {
+  return boardMatrix.map((row, y) => {
+    if (yx[0] === y) {
+      return row.map((space, x) => {
+        if (yx[1] === x && space[0] === "N") {
+          onSuccessCb();
+          return [playerTurn, null];
+        } else {
+          return space;
+        }
+      });
+    } else {
+      return row;
+    }
+  });
 };
 
 // Get value and group info for 4 surrounding spaces
-export const getSurroundingInfo = (boardMatrix: BoardMatrix, yx: Coordinates) => {
+export const getSurroundingInfo = (
+  boardMatrix: BoardMatrix,
+  yx: Coordinates
+) => {
   const surroundingInfo = [];
   surroundingInfo.push(getSpaceInfo(boardMatrix, [yx[0] - 1, yx[1]]));
   surroundingInfo.push(getSpaceInfo(boardMatrix, [yx[0], yx[1] + 1]));
   surroundingInfo.push(getSpaceInfo(boardMatrix, [yx[0] + 1, yx[1]]));
   surroundingInfo.push(getSpaceInfo(boardMatrix, [yx[0], yx[1] - 1]));
   return surroundingInfo;
-}
+};
 
 // Private functions
 
@@ -25,4 +59,4 @@ const getSpaceInfo = (boardMatrix: BoardMatrix, yx: Coordinates) => {
   } catch {
     return undefined;
   }
-}
+};
