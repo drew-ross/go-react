b7c533486148e63a399267d2b83745ed1130753b
Drew <mail.drew.ross@gmail.com>
2021-07-21 08:34:11 -0700

    Add getSurroundingInfo, getSpaceInfo functions

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,28 @@
-import { BoardMatrix, BoardSpace } from "./types/gameTypes";
+import { BoardMatrix, BoardSpace, Coordinates } from "./types/gameTypes";
+
+// Public functions
 
 export const constructBoardMatrix = (size: number = 19): BoardMatrix => {
   return new Array(size).fill(
     new Array<BoardSpace>(size).fill(["N", null], 0, size), 0, size);
 };
+
+// Get value and group info for 4 surrounding spaces
+export const getSurroundingInfo = (boardMatrix: BoardMatrix, space: Coordinates) => {
+  const surroundingInfo = [];
+  surroundingInfo.push(getSpaceInfo(boardMatrix, [space[0] - 1, space[1]]));
+  surroundingInfo.push(getSpaceInfo(boardMatrix, [space[0], space[1] + 1]));
+  surroundingInfo.push(getSpaceInfo(boardMatrix, [space[0] + 1, space[1]]));
+  surroundingInfo.push(getSpaceInfo(boardMatrix, [space[0], space[1] - 1]));
+  return surroundingInfo;
+}
+
+// Private functions
+
+const getSpaceInfo = (boardMatrix: BoardMatrix, space: Coordinates) => {
+  try {
+    return boardMatrix[space[0]][space[1]];
+  } catch {
+    return undefined;
+  }
+}
