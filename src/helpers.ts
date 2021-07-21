761787847e31327b12f352cd77f376c443ee72d4
Drew <mail.drew.ross@gmail.com>
2021-07-21 12:43:07 -0700

    Add logic to group functions

--- src/helpers.ts
+++ src/helpers.ts
@@ -84,14 +84,76 @@ export const getSpaceInfo = (
   }
 };
 
-export const createGroup = (groups: Groups) => {};
+// Create a new group, return [new groups object, new group number]
+export const createGroup = (
+  groups: Groups
+): { groups: Groups; added: number | undefined } => {
+  for (let i = 0; i < 1000; i++) {
+    if (!groups.hasOwnProperty(i)) {
+      return { groups: { ...groups, [i]: [] }, added: i };
+    }
+  }
+  return { groups, added: undefined };
+};
 
 export const addToGroup = (
   groups: Groups,
   boardMatrix: BoardMatrix,
   groupNumber: number,
   yx: Coordinates
+): [Groups, BoardMatrix] => {
+  if (
+    groups.hasOwnProperty(groupNumber) &&
+    boardMatrix[yx[0]] !== undefined &&
+    boardMatrix[yx[0]][yx[1]] !== undefined
+  ) {
+    return [
+      groups,
+      boardMatrix.map((row, y) => {
+        if (yx[0] === y) {
+          return row.map((space, x) => {
+            if (yx[1] === x) {
+              return [space[0], groupNumber];
+            } else {
+              return space;
+            }
+          });
+        } else {
+          return row;
+        }
+      }),
+    ];
+  } else {
+    return [groups, boardMatrix];
+  }
+};
+
+// Change space group number in boardMatrix, return new boardMatrix
+export const updateSpaceGroup = (
+  boardMatrix: BoardMatrix,
+  groupNumber: number
 ) => {};
+
+// Add space coordinates to groups, return new groups
+export const addSpaceToGroup = (
+  groups: Groups,
+  yx: Coordinates,
+  groupNumber: number | undefined
+): Groups => {
+  if (groupNumber !== undefined && groups.hasOwnProperty(groupNumber)) {
+    const newGroups: Groups = {};
+    for (let key in groups) {
+      if (Number(key) === groupNumber) {
+        newGroups[Number(key)] = [...groups[key], yx];
+      } else {
+        newGroups[Number(key)] = groups[key];
+      }
+    }
+    return newGroups;
+  } else {
+    return groups;
+  }
+};
 
 export const combineGroups = (
   groups: Groups,
