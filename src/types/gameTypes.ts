cf98a0c2c3a5a78bfddaf2539fb90fcf9476cf04
Drew <mail.drew.ross@gmail.com>
2021-07-27 12:45:32 -0700

    Update BoardSpace comment

--- src/types/gameTypes.ts
+++ src/types/gameTypes.ts
@@ -1,6 +1,10 @@
-// Board spaces can be empty (N for null or no piece),
-// have a black piece (B)
-// or a white piece (W)
+/* BoardSpaces have information about the space in the form of
+[spaceColor, groupNumber]
+Board spaces can be empty (N), have a black piece (B) or a white piece (W).
+null values for spaceColor are for spaces which can't be found
+null values for groupNumber are for (N) spaces that don't belong to a group
+All (B) or (W) spaces should also have a group number, even if they are alone
+*/ 
 export type BoardSpace = ["N" | PieceColor | null, number | null];
 
 export type PieceColor = "B" | "W";
