e7aada858d8475513e9ad1350ba28da2eee01e6d
Drew <mail.drew.ross@gmail.com>
2021-07-27 12:04:00 -0700

    Update BoardSpace type, add MetaSpace interface

--- src/types/gameTypes.ts
+++ src/types/gameTypes.ts
@@ -1,7 +1,7 @@
 // Board spaces can be empty (N for null or no piece),
 // have a black piece (B)
 // or a white piece (W)
-export type BoardSpace = ["N" | PieceColor, number | null];
+export type BoardSpace = ["N" | PieceColor | null, number | null];
 
 export type PieceColor = "B" | "W";
 
@@ -10,3 +10,8 @@ export type BoardMatrix = BoardSpace[][];
 export type Coordinates = [number, number];
 
 export type Groups = { [key: number]: Coordinates[] };
+
+export interface MetaSpace {
+  space: BoardSpace;
+  yx: Coordinates;
+}
