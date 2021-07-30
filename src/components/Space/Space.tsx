9ef793542be434e8a339f3d8380282904ace1960
Drew <mail.drew.ross@gmail.com>
2021-07-30 13:32:37 -0700

    Add div to display stones on hover for user affordance

--- src/components/Space/Space.tsx
+++ src/components/Space/Space.tsx
@@ -1,17 +1,18 @@
 import { ReactElement } from "react";
-import { BoardSpace, Coordinates } from "../../types/gameTypes";
+import { BoardSpace, Coordinates, PieceColor } from "../../types/gameTypes";
 
 import "./Space.css";
 
 interface SpaceProps {
   value: BoardSpace;
+  playerTurn: PieceColor;
   yx: Coordinates;
   handleMove: (yx: Coordinates) => void;
   showDebug: boolean;
 }
 
 const Space = (props: SpaceProps): ReactElement => {
-  const { value, yx, handleMove, showDebug } = props;
+  const { value, yx, handleMove, showDebug, playerTurn } = props;
 
   const handleClick = () => {
     handleMove(yx);
@@ -22,6 +23,9 @@ const Space = (props: SpaceProps): ReactElement => {
       <div className='line-h' />
       <div className='line-v' />
       <div className='stone-display' />
+      {value[0] === "N" && (
+        <div className={`hover-stone-display ${playerTurn}`} />
+      )}
       {showDebug && (
         <span className='debug-display'>{`${yx[0]}, ${yx[1]}`}</span>
       )}
