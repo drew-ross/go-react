f36259dcdc274893806eb54cd1fe4279423ef0db
Drew <mail.drew.ross@gmail.com>
2021-08-01 10:24:05 -0700

    Add h1, render Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import "./App.css";
 import { ReactElement } from "react";
 
 import Board from './components/Board/Board';
+import Footer from "./components/Footer/Footer";
 
 const App = (): ReactElement => {
   return (
     <main className='App'>
+      <h1>Go-React</h1>
       <Board />
+      <Footer />
     </main>
   );
 };
